5b5339eb8084b6a38356717aaa20fe5efe9549d0
satyaperumalla <satyaperumalla09@gmail.com>
2022-07-23 21:23:00 +0530

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'forms';
+export class AppComponent implements OnInit {
+  form!: FormGroup;
+  submitted: boolean = false;
+
+  ngOnInit(){
+    this.form = new FormGroup({
+      personalDetails:new FormGroup({
+        firstName: new FormControl('',Validators.required),
+        lastName:new FormControl('',Validators.required),
+        email:new FormControl('')
+      }),
+      gender:new FormControl('male'),
+      skillSet:new FormArray([
+        new FormControl(null,Validators.required)
+      ])
+    })
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    console.log(this.form);
+  }
+
+  get skillSet () {
+    return this.form.get('skillSet') as FormArray
+  }
+
+  get personalDetails(){
+    return this.form.get('personalDetails') as FormGroup
+  }
+
+  AddSkill(){
+    console.log("addskill is called");
+    this.skillSet.push(new FormControl(null,Validators.required))
+  }
+
+  deleteFormControl(i:number){
+    console.log("delete is called")
+    this.skillSet.removeAt(i);
+  }
 }
+
